3fdd13baa1741aaaade40e811cec2ee69667b7ae
Nick Drishinski <ndrishinski@yahoo.com>
2018-04-28 06:15:14 -0600

    Styled headers added mixins

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
-import BirthdayForm from './birthdayform';
+import BirthdayForm from './birthdayForm';
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <BirthdayForm />
+        <header>
+          <h1>Birthday Countdown</h1>
+          <div className="header__skew">
+              <div className="header__subskew">
+                 <BirthdayForm />
+              </div>
+          </div>
+        </header>
       </div>
     );
   }
